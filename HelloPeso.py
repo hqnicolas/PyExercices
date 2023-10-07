0816f503e9943a6898ea3a4ce1d8e09abd6cbb95
Nicolas Pereira <41456803+hqnicolas@users.noreply.github.com>
2023-10-07 20:17:24 -0300

    Update HelloPeso.py

--- HelloPeso.py
+++ HelloPeso.py
@@ -1,5 +1,5 @@
-nome = input ('Qual é seu nome: ')
+nome = input ('Qual é seu nome: ')                             # Esta é minha primeira linha de código escrita em Python3
 peso = input ('Qual é seu peso: ')
 idade = input ('Qual é sua idade?: ')
-print ('nome: ',nome,', idade,',idade,', peso',peso)
+print ('nome: ',nome,', idade,',idade,', peso',peso)           # Aqui podemos notar que eu ainda tenho o cacuete da formatação Python2 herdada da linguagem C 
 hello = input ('Pressione Enter')
