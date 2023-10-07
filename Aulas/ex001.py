7a6fc07957da712cc1082cdbe1ca5c90556e6ac8
Nicolas Pereira <41456803+hqnicolas@users.noreply.github.com>
2023-10-07 20:08:39 -0300

    Adicionados Exercícios das Aulas

---
+++ Aulas/ex001.py
@@ -0,0 +1,7 @@
+######################################################################################
+#  Script Python escrito em 07/10/2023 por Nicolas Pereira no Jetbrains Pycharm      #
+#  Exercício Numero '1' do curso cursoemvideo.com                                    #
+######################################################################################
+print(' Olá Mundo! ')                # Exibe no terminal a frase "olá mundo"
+msg = ' Olá mundo! '                 # Coloca dentro de uma variável string a frase 'Olá mundo!'
+print(msg)                           # Exibe na tela a frase guardada dentro da string 'msg'
