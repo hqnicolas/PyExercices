6e5ed244a09ce920f61396e4ee2a41f5b6e3c75b
Nicolas Pereira <41456803+hqnicolas@users.noreply.github.com>
2023-10-08 17:20:18 -0300

    Delete Aula/ex012.py

--- Aula/ex012.py
+++
@@ -1,8 +0,0 @@
-######################################################################################
-#  Script Python escrito em 08/10/2023 por Nicolas Pereira no Jetbrains Pycharm      #
-#  Exercício Numero '12' do curso cursoemvideo.com da aula 7 de operadores           #
-#  Operadores Aritiméticos: ||| + ||| - | * ||| / ||| ** ||| // ||| % ||| == |||     #
-#  Simbolo de igualdade para resultados  ==                                          #
-######################################################################################
-# um script que leia um numero inteiro qualquer e mostre na tela a sua tabuada       #
-######################################################################################
