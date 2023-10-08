4495d0c34a3f1a612a434899e7bd6789c3ccb9ae
Nicolas Pereira <41456803+hqnicolas@users.noreply.github.com>
2023-10-07 23:16:52 -0300

    Adicionados Exercícios das Aulas

---
+++ Aulas/ex008.py
@@ -0,0 +1,6 @@
+######################################################################################
+#  Script Python escrito em 07/10/2023 por Nicolas Pereira no Jetbrains Pycharm      #
+#  Exercício Numero '4' do curso cursoemvideo.com                                    #
+#  Operadores Aritiméticos: ||| + ||| - | * ||| / ||| ** ||| // ||| % ||| == |||     #
+#  Simbolo de igualdade para resultados  ==                                          #
+######################################################################################
