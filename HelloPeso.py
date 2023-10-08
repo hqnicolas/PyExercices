56ffeffeb777592cf6377502e1ae896bf3d93baa
Nicolas Pereira <41456803+hqnicolas@users.noreply.github.com>
2023-10-07 23:46:21 -0300

    Delete HelloPeso.py

--- HelloPeso.py
+++
@@ -1,5 +0,0 @@
-nome = input ('Qual é seu nome: ')                             # Esta é minha primeira linha de código escrita em Python3
-peso = input ('Qual é seu peso: ')
-idade = input ('Qual é sua idade?: ')
-print ('nome: ',nome,', idade,',idade,', peso',peso)           # Aqui podemos notar que eu ainda tenho o cacuete da formatação Python2 herdada da linguagem C 
-hello = input ('Pressione Enter')
