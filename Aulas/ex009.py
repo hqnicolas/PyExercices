8505fb25e2c4768b7569843951763d36616b9033
Nicolas Pereira <41456803+hqnicolas@users.noreply.github.com>
2023-10-08 12:50:30 -0300

    Adicionados Exercícios das Aulas

---
+++ Aulas/ex009.py
@@ -0,0 +1,13 @@
+######################################################################################
+#  Script Python escrito em 08/10/2023 por Nicolas Pereira no Jetbrains Pycharm      #
+#  Exercício Numero '9' do curso cursoemvideo.com da aula 7 de operadores            #
+#  Operadores Aritiméticos: ||| + ||| - | * ||| / ||| ** ||| // ||| % ||| == |||     #
+#  Simbolo de igualdade para resultados  ==                                          #
+######################################################################################
+# desenvolva um programa que leia as duas notas de um aluno, calcule e moste a media #
+######################################################################################
+print('Calculadora de média')                                    # Apresentação do Script
+n1 = float(input('digite a nota da Primeira Avaliação: '))       # Solicita a nota para a variável float "n1"
+n2 = float(input('digite a nota da Segunda Avaliação: '))        # Solicita a nota para a variável float "n2"
+r = ((n1 + n2) / 2)                                              # Efetua o calculo da média
+print('A média deste aluno foi: {}' .format(r))                  # Retorna o resultado
---
+++ Aulas/ex010.py
@@ -0,0 +1,17 @@
+######################################################################################
+#  Script Python escrito em 08/10/2023 por Nicolas Pereira no Jetbrains Pycharm      #
+#  Exercício Numero '9' do curso cursoemvideo.com da aula 7 de operadores            #
+#  Operadores Aritiméticos: ||| + ||| - | * ||| / ||| ** ||| // ||| % ||| == |||     #
+#  Simbolo de igualdade para resultados  ==                                          #
+######################################################################################
+# leia um valor em metros e o exiba comvertido em centimetros e milimetros           #
+######################################################################################
+print('Metros para centimetros! ')                   # Print em tela da descrição do script
+print('use . ao invez de , ')
+mt = float(input('Digite o Valor em Metros: '))      # Recebe o Valor em metros com virgola
+nc = mt * 100                                        # calcula em centimetros
+nci = int(nc)                                        # converte para um numero inteiro em centimetros
+nm = mt * 1000                                       # calcula em milimetros
+nmi = int(nm)                                        # converte para um numero inteiro em milimetros
+print('O Valor em Centimetros é: {}'.format(nci))    # exibe o valor em centimetros
+print('O Valor em milimetros é: {}'.format(nmi))     # exibe o valor em milimetros
