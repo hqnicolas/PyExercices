97366cc786b75dbb8344d9fa4a20d570f32bf841
Nicolas Pereira <41456803+hqnicolas@users.noreply.github.com>
2023-10-08 19:41:02 -0300

    Delete Aula directory

--- Aula/ex002.py
+++
@@ -1,7 +0,0 @@
-######################################################################################
-#  Script Python escrito em 07/10/2023 por Nicolas Pereira no Jetbrains Pycharm      #
-#  Exercício Numero '2' do curso cursoemvideo.com                                    #
-######################################################################################
-nome = input('Digite seu nome: ')                    # o comando input recebe uma variavel string por padrão
-print('É um prazer te conhecer,',nome + '!')         # útilizando metodo de Print da linguagem Python2
-print('É um prazer te conhecer, {}!'.format(nome))   # o comando .format transforma a variavel para caber em uma string
